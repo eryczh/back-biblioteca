dfaa9170c998f5705c9f46e18bb5c11966253496
eryczh <ericandrade2810@gmail.com>
2024-05-31 19:50:12 -0300

    erros corrigidos

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import 'dotenv/config'
 import userController from './controller/userController.js';
 import roupasController from './controller/roupasController.js';
 
+
 import express from 'express';
 import cors from 'cors';
 
@@ -13,5 +14,9 @@ server.use(express.json());
 server.use(userController);
 server.use(roupasController);
 
+server.use('/storage/roupa', express.static('storage/roupa'));
+
 let port = process.env.PORT;
 server.listen(port, () => console.log("API SUBIU!"));
+
+
